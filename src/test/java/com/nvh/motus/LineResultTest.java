a263f52d4d7b2bc90c3c2773ca2b1fae9bb4557b
Nicolas Vanhauter <nvhfr@yahoo.fr>
2016-08-18 14:10:52 +0200

    chore(test): Implémente les tests unitaires

---
+++ src/test/java/com/nvh/motus/LineResultTest.java
@@ -0,0 +1,39 @@
+package com.nvh.motus;
+
+import com.nvh.motus.model.Motus;
+import com.nvh.motus.service.Dictionaries;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class LineResultTest {
+
+    private Motus test;
+
+    @Before
+    public void setup() {
+
+        new Dictionaries();
+        test = new Motus(6, false);
+    }
+
+    @Test
+    public void shouldMatchCorrectLineResult() {
+        //valide le bon fonctionnement de la réponse
+        test.getCurrentTurn().setWordToFind("PATRON");
+        List<Integer> found = test.getCurrentTurn().lineResult("PRETER");
+        List<Integer> expected = new ArrayList<>();
+        expected.add(2);
+        expected.add(1);
+        expected.add(0);
+        expected.add(1);
+        expected.add(0);
+        expected.add(0);
+        assertEquals(found, expected);
+
+    }
+}
